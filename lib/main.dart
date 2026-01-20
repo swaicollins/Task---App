9045f6b38b32ba85d4f2a65e4c0bef6c40a170ef
Collins Swai <swailerc23@gmail.com>
2026-01-20 09:22:23 +0300

    Main entry for app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:task/presentation/register_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RegisterScreen(),
+    );
+  }
+}
