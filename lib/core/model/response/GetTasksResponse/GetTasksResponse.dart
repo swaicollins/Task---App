ca76e4dccf6e17ca4f5291937dcc38dd41a46bbc
Collins Swai <swailerc23@gmail.com>
2026-01-20 09:36:16 +0300

    Get tasks response json

---
+++ lib/core/model/response/GetTasksResponse/GetTasksResponse.dart
@@ -0,0 +1,24 @@
+class GetTasksResponse {
+  int? id;
+  String? title;
+  String? description;
+  bool? completed;
+
+  GetTasksResponse({this.id, this.title, this.description, this.completed});
+
+  GetTasksResponse.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    description = json['description'];
+    completed = json['completed'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['completed'] = this.completed;
+    return data;
+  }
+}
