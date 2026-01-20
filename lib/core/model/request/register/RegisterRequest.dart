9685f46dfef0269b073eb1762324013765cf0605
Collins Swai <swailerc23@gmail.com>
2026-01-20 09:33:04 +0300

    Register request json

---
+++ lib/core/model/request/register/RegisterRequest.dart
@@ -0,0 +1,18 @@
+class RegisterRequest {
+  String? username;
+  String? password;
+
+  RegisterRequest({this.username, this.password});
+
+  RegisterRequest.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    password = json['password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['username'] = this.username;
+    data['password'] = this.password;
+    return data;
+  }
+}
