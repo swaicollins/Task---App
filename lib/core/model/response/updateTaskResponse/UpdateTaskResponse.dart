9606db141509e6fbb294b2ff393b66ec965051c8
Collins Swai <swailerc23@gmail.com>
2026-01-20 09:38:19 +0300

    Update task response json

---
+++ lib/core/model/response/updateTaskResponse/UpdateTaskResponse.dart
@@ -0,0 +1,24 @@
+class UpdateTaskResponse {
+  int? id;
+  String? title;
+  String? description;
+  bool? completed;
+
+  UpdateTaskResponse({this.id, this.title, this.description, this.completed});
+
+  UpdateTaskResponse.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    description = json['description'];
+    completed = json['completed'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['completed'] = this.completed;
+    return data;
+  }
+}
