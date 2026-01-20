002db3801daed249b89715c6dd2bfaa961477acd
Collins Swai <swailerc23@gmail.com>
2026-01-20 09:35:35 +0300

    register response json

---
+++ lib/core/model/response/RegisterResponse/RegisterResponse.dart
@@ -0,0 +1,15 @@
+class RegisterResponse {
+  String? message;
+
+  RegisterResponse({this.message});
+
+  RegisterResponse.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    return data;
+  }
+}
