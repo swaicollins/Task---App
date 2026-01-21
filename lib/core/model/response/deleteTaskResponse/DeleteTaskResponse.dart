6bd1f3eeaa05b4e98af1631ac3a4ca873b4de8bd
Collins Swai <swailerc23@gmail.com>
2026-01-21 12:08:09 +0300

    Delete task json

---
+++ lib/core/model/response/deleteTaskResponse/DeleteTaskResponse.dart
@@ -0,0 +1,15 @@
+class DeleteTaskResponse {
+  String? message;
+
+  DeleteTaskResponse({this.message});
+
+  DeleteTaskResponse.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    return data;
+  }
+}
